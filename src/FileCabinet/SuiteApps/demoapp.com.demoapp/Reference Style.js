3519307e5b4b5741ec717df1dc2cdae50997ec84
Marcel Dumitrescu <marcel.dumitrescu@gmail.com>
2021-11-09 11:18:38 +0000

    Uplaod Initial Files

---
+++ src/FileCabinet/SuiteApps/demoapp.com.demoapp/Reference Style.js
@@ -0,0 +1,14 @@
+        /**
+        * @function function_name
+        * @description
+        *
+        * @param {string}
+        * @param {string}
+        * @return {boolean}
+        */
+
+        let function_name;
+        function_name = (p1, p2) => {
+
+            return "";
+        };
